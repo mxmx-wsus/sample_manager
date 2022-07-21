86c39d00d98dcd44427256973545766882563157
RyoutaMasuyama <mxmx.khaki@gmail.com>
2022-07-21 20:18:39 +0900

    リポジトリ構成の修正

--- lib/app.dart
+++ lib/app.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
-import 'text_data.dart';
-import 'package:sample_manager/view/app_title.dart';
+import 'package:sample_manager/view_model/app_title.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: AppTitle(),
     );
   }
